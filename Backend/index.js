8ad7158bda9667b5805e32197612d24eab4e8420
aazacz <abhilashabinz@gmail.com>
2024-09-18 00:04:57 +0530

    added some changes in the backend

--- Backend/index.js
+++ Backend/index.js
@@ -3,24 +3,24 @@ const app           = express()
 const cors          = require('cors')
 const cookieparser  = require('cookie-parser')
 require('dotenv').config()
-const {userRoute}     = require('./Router/UserRoute');// Import user routes
+// const {userRoute}     = require('./Router/UserRoute');
 require("./Config/mongodbServer")                     // Connect to MongoDB
 
 
 const PORT = process.env.PORT
-
+ 
 app.use(cors())// Enable CORS
 app.use(cookieparser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 // Routes
-app.use('/user', userRoute)
+// app.use('/user', userRoute)
 
 // Homepage route
 app.get("/",(req,res)=>{
-    res.status(200).json({Success:"Welcome, This is the success response for HomePage"})
-                        })
+    res.status(200).json({Success:"Welcome,  This is the success response for HomePage"})
+                        }) 
  
 
 // Start the server
